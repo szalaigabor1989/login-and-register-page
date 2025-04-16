cb570928beeef683f99bcac7d2b22eeb97e8c994
Szalai Gábor <szalai.gabor.1989@gmail.com>
2025-04-16 22:54:05 +0200

    mod

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
-const formContainer = document.querySelector('.form-container'),
-      loginLink = document.querySelector('.login-link'),
-      signupLink = document.querySelector('.signup-link');
+document.addEventListener('DOMContentLoaded', () => {
+    const formContainer = document.querySelector('.form-container'),
+        loginLink = document.querySelector('.login-link'),
+        signupLink = document.querySelector('.signup-link');
 
-if (formContainer && loginLink && signupLink) {
-    signupLink.addEventListener('click', () => {
-        formContainer.classList.add('signup');
-    });
-
-    loginLink.addEventListener('click', () => {
-        formContainer.classList.remove('signup');
-    });
-}
-
-const rippleButtons = document.querySelectorAll('.btn-ripple');
-if (rippleButtons.length) {
-    rippleButtons.forEach(rippleButton => {
-        rippleButton.addEventListener('click', event => {
-            let x = event.clientX - event.target.getBoundingClientRect().left;
-            let y = event.clientY - event.target.getBoundingClientRect().top;
-
-            let ripples = document.createElement('span');
-            ripples.style.left = (x - 20) + 'px';
-            ripples.style.top = (y - 20) + 'px';
-            event.target.appendChild(ripples);
-
-            setTimeout(() => {
-                ripples.remove();
-            }, 500);
+    if (formContainer && loginLink && signupLink) {
+        signupLink.addEventListener('click', () => {
+            formContainer.classList.add('signup');
         });
-    });
-}
+
+        loginLink.addEventListener('click', () => {
+            formContainer.classList.remove('signup');
+        });
+    }
+
+    const rippleButtons = document.querySelectorAll('.btn-ripple');
+    if (rippleButtons.length) {
+        rippleButtons.forEach(rippleButton => {
+            rippleButton.addEventListener('click', event => {
+                let x = event.clientX - event.target.getBoundingClientRect().left;
+                let y = event.clientY - event.target.getBoundingClientRect().top;
+
+                let ripples = document.createElement('span');
+                ripples.style.left = (x - 20) + 'px';
+                ripples.style.top = (y - 20) + 'px';
+                event.target.appendChild(ripples);
+
+                setTimeout(() => {
+                    ripples.remove();
+                }, 500);
+            });
+        });
+    }
+});
